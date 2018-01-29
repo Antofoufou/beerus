ee892643ef6c4dd7d2cbd4254b6fee5532a8c291
Antofoufou <35931707+Antofoufou@users.noreply.github.com>
2018-01-29 22:33:11 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.user.setGame("Hakai, .help");
 console.log("Le bot a bien été connecté");
 });
 
-bot.login("NDA3NTc2MDgxOTIzNzAyNzk0.DVDg8g.KlsqxbXYV4OLBGG60ELlV9zDlms");
+bot.login("process.env.TOKEN");
 
 bot.on("message", message => {
     if (message.content === prefix + "help"){
