95c9579a4bafa1652d7baf437a5579b4c10588e4
Antofoufou <35931707+Antofoufou@users.noreply.github.com>
2018-01-29 21:48:55 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,3 +16,26 @@ bot.on("message", message => {
         console.log("Commande Help demandée !");
     }
 });    
+
+    switch (args[0].toLowerCase()){
+
+        case "kick":
+
+        if (!message.channel.permissionsFor(message.member).hasPermission("KICK_MEMBERS"))
+            message.reply("Les pouvoirs du Dieu de la Destruction ne sont autorisés qu'aux Modérateur, chose que tu sembles loin d'être.")
+        }else{
+            var member = message.mentions.members.firsts();
+            if(!member){
+                message.reply("Je ne trouve pas la personne à détruire ..");
+            }else{
+                if(!member.kickable)
+                    message.reply("Mon pouvoir n'est pas assez rechargé pour ça.")
+                }else{
+                    member.kick().then((member) => {
+                    message.channel.send("${member.displayName} à été annihilé, adieu.");
+                }).catch() => {
+                    message.channel.send("Kick refusé")
+            }    
+        }
+
+        break;
