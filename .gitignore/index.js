33f096c1ede59e3dc1e5c60c23f87dbdac04cc27
Antofoufou <35931707+Antofoufou@users.noreply.github.com>
2018-01-29 20:44:00 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 var bot = new Discord.Client();
+var prefix - (".");
 
 bot.on("ready", function() {
 bot.user.setGame("Hakai, .help");
@@ -8,3 +9,8 @@ console.log("Le bot a bien été connecté");
 });
 
 bot.login("NDA3NTc2MDgxOTIzNzAyNzk0.DVDg8g.KlsqxbXYV4OLBGG60ELlV9zDlms");
+
+bot.on("meessage", message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Voici les commandes bande d'enculés :\n .help pour afficher les commandes");
+        console.log("Commande Help demandée !");
